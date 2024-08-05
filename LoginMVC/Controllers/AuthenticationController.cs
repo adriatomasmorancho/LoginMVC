300c05c567919c8eabc9e0b8fb5773ee9c56fd8b
Adria Tomas <adriatomasmorancho@gmail.com>
2024-08-05 23:12:18 +0200

    Agregar archivos de proyecto.

---
+++ LoginMVC/Controllers/AuthenticationController.cs
@@ -0,0 +1,46 @@
+﻿using LoginMVC.DAL;
+using LoginMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginMVC.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+
+        // GET: Authentication
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model) {
+
+            ViewBag.Error = "Algo ha fallado";
+
+            if (ModelState.IsValid)
+            {
+                UsuarioDAL usuario = new UsuarioDAL();
+
+                List<LoginViewModel> list = usuario.GetUserListFromDB();
+
+                foreach (var item in list)
+                {
+                    if(item.Email.Trim() == model.Email && item.Password.Trim() == model.Password)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            return View(model);
+        }
+
+
+    }
+}
---
+++ LoginMVC/DAL/UsuarioDAL.cs
@@ -0,0 +1,84 @@
+﻿using LoginMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace LoginMVC.DAL
+{
+    public class UsuarioDAL
+    {
+        private DbConnection connection;
+        public UsuarioDAL()
+        {
+            connection = new DbConnection();
+        }
+
+        public List<LoginViewModel> GetUserListFromDB()
+        {
+            List<LoginViewModel> jobs = new List<LoginViewModel>();
+
+            connection.Open();
+            try
+            {
+
+                string query = "SELECT * FROM Usuarios;";
+                SqlCommand cmd = new SqlCommand(query, connection.GetConnection());
+
+                // Recuperamos un lector...
+                SqlDataReader records = cmd.ExecuteReader();
+
+                while (records.Read())
+                {
+                    LoginViewModel user = new LoginViewModel();
+                    user.Email = records.GetString(records.GetOrdinal("Email"));
+                    user.Password = records.GetString(records.GetOrdinal("Password"));
+
+                    // Agrega más campos según la estructura de tu tabla y tu clase Job
+                    jobs.Add(user);
+                }
+                connection.Close();
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return jobs;
+        }
+
+        public void InsertUserToDB(LoginViewModel newUser)
+        {
+            try
+            {
+                this.connection.Open();
+                string sql = @"INSERT INTO Usuarios( 
+                                             Email, 
+                                             Password) 
+
+                                VALUES (@Email, 
+                                        @Password
+                                        )";
+
+
+                using (SqlCommand cmd = new SqlCommand(sql, this.connection.GetConnection()))
+                {
+                    cmd.Parameters.AddWithValue("@Email", newUser.Email);
+                    cmd.Parameters.AddWithValue("@Password", newUser.Password);
+
+
+                    cmd.ExecuteNonQuery();
+                }
+                this.connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
